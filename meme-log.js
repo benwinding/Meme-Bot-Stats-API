5d20d1f077f630a5cd32837b7293130d69c3f1dd
Ben Winding <ben.winding@gmail.com>
2018-05-26 01:34:05 +0930

    Fix

--- meme-log.js
+++ meme-log.js
@@ -12,7 +12,8 @@ const creds_json = {
     private_key: getPrivateKey(),
 };
 
-const totalRows = 14;
+const maxRows = 30;
+const numCols = 2;
 
 module.exports = {
     ReturnTable: ReturnTable,
@@ -20,12 +21,16 @@ module.exports = {
     GetCount: GetCount,
 };
 
+const MemeTable = function () {
+
+};
+
 function ReturnTable() {
     return GetSpreadsheetDoc()
         .then((res) => Authenticate(res))
         .then((doc) => PrintInfo(doc))
         .then((doc) => GetSheetFromDoc(doc, 0))
-        .then((sheet) => GetTable(sheet, totalRows, 2))
+        .then((sheet) => GetTableAsArray(sheet, maxRows, numCols))
         .then((table) => SimplifyTable(table))
         .then((sTable) => Promise.resolve(sTable))
         .catch((err) => Promise.reject("!GetTable failed: " + err));
@@ -36,7 +41,7 @@ function IncrementCounter(key) {
         .then((res) => Authenticate(res))
         .then((doc) => PrintInfo(doc))
         .then((doc) => GetSheetFromDoc(doc, 0))
-        .then((sheet) => GetTable(sheet, totalRows, 2))
+        .then((sheet) => GetTableAsArray(sheet, maxRows, numCols))
         .then((table) => IncrementKey(table, key))
         .then((table) => SimplifyTable(table))
         .then((sTable) => Promise.resolve(sTable))
@@ -48,7 +53,7 @@ function GetCount(key) {
         .then((res) => Authenticate(res))
         .then((doc) => PrintInfo(doc))
         .then((doc) => GetSheetFromDoc(doc, 0))
-        .then((sheet) => GetTable(sheet, totalRows, 2))
+        .then((sheet) => GetTableAsArray(sheet, maxRows, numCols))
         .then((table) => SimplifyTable(table))
         .then((sTable) => Promise.resolve(sTable[key]))
         .catch((err) => Promise.reject("!GetCount failed: " + err));
@@ -97,54 +102,66 @@ function GetSheetFromDoc(doc, sheetNum) {
                 return;
             }
             const sheet = info.worksheets[sheetNum];
-            log(`sheet 1: ${sheet.title} ${sheet.rowCount}x${sheet.colCount}`);
+            log(`Got sheet, name:${sheet.title}, size:${sheet.rowCount}x${sheet.colCount}`);
             resolve(sheet);
         });
     });
 }
 
-function GetTable(sheet, rowCount, colCount) {
-    const tableObj = {};
+function GetTableAsArray(sheet, rowCount, colCount) {
     return new Promise((resolve, reject) => {
-        sheet.getCells({'max-row': rowCount, 'max-col': colCount}, (err, cells) => {
+        sheet.getCells({'max-row': rowCount, 'max-col': colCount, 'return-empty': true}, (err, cells) => {
             if (err) {
                 reject(err.toString());
                 logErr("! Error GetTable: " + err.toString());
                 return;
             }
+            const table = [];
             for(let r=0; r< rowCount; r++) {
-                let rowKey = cells[r * 2].value;
-                let rowValue = cells[r * 2 + 1];
-                tableObj[rowKey] = rowValue;
+                const cellLeft = cells[r * 2];
+                const cellRight = cells[r * 2 + 1];
+                let row = {};
+                row.name = cellLeft;
+                row.counter = cellRight;
+                table.push(row);
             }
             log("Got table from sheet");
-            resolve(tableObj);
+            resolve(table);
         });
     });
 }
 
 function SimplifyTable(table) {
-    const simpleTable = {};
     log("simplifying table ...");
-    for(let key in table) {
-        simpleTable[key] = table[key].value;
+    const simpleTable = {};
+    for (const row of table) {
+        if (row.name.value)
+            simpleTable[row.name.value] = row.counter.value;
     }
     return Promise.resolve(simpleTable);
 }
 
-function IncrementKey(table, key) {
-    return new Promise((resolve, reject) => {
-        log("Incrementing key:" +  key);
-        if(!table[key]) {
-            reject(`No key named: '${key}'`);
-            logErr("! Error IncrementKey, no key named: " + key);
-            return;
+function IncrementKey(table, name) {
+    for (const row of table) {
+        if (row.name && row.name.value === name) {
+            row.counter.value = Number(row.counter.value) + 1;
+            row.counter.save();
+            log("Incrementing key: " +  name);
+            return Promise.resolve(table);
         }
-        table[key].value = Number(table[key].value) + 1;
-        log("Saving result");
-        table[key].save();
-        resolve(table);
-    })
+    }
+    for (const row of table) {
+        if (row.name.value)
+            continue;
+        row.name.value = name;
+        row.name.save();
+        row.counter.value = 1;
+        row.counter.save();
+        log("Adding new key: " + name);
+        return Promise.resolve(table);
+    }
+    logErr("! Error IncrementKey, unable to add key: " + name);
+    return Promise.reject(`! Error IncrementKey, unable to add key: '${name}'`);
 }
 
 function log(message) {
